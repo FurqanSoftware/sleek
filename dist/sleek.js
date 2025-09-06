3479ac8dc64aafc41aeccb56e62ee3e36f61c0d0
Mahmud Ridwan <m@hjr265.me>
2025-09-06 18:17:02 +0600

    Update built CSS

--- dist/sleek.js
+++ dist/sleek.js
@@ -373,6 +373,9 @@ class Dropdown {
   renderToggleSelect() {
     const toggle = dom.$('.dropdown__toggle', this.el);
     toggle.innerHTML = '';
+    if (dom.hasClass(toggle, 'form__field')) {
+      if (dom.hasClass(this.el, '-readonly')) toggle.setAttribute('readonly', true);else toggle.removeAttribute('readonly');
+    }
     const select = dom.$('select', this.el);
     if (select.selectedOptions.length == 0 || select.selectedOptions.length == 1 && select.selectedOptions[0].dataset.empty) {
       this.renderTogglePlaceholder();
@@ -433,6 +436,7 @@ class Dropdown {
     const data = this.extractOptionData(option);
     const item = this.makeItem(data);
     dom.on(item, 'click', () => {
+      if (dom.hasClass(this.el, '-readonly')) return;
       if (option.dataset.empty) {
         const currentSelected = [...select.selectedOptions];
         option.selected = true;
